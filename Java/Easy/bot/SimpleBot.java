44a66b1c2413906936ca59968ad63ceeda9d83bb
Jiaxing <119725494+Diariodelasislas@users.noreply.github.com>
2023-07-04 13:51:17 +0800

    Update SimpleBot.java

--- Java/Easy/bot/SimpleBot.java
+++ Java/Easy/bot/SimpleBot.java
@@ -6,7 +6,7 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
+        greet("LysanderOliviaLiBot", "2023/7/4"); // change it as you need
         remindName();
         guessAge();
         count();
